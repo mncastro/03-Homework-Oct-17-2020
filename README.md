9ea38a0c5e788f63839203a46c5895cb82fc903f
¨Mario Castro¨ <mnc634@utexas.edu>
2020-10-31 19:18:44 -0600

    updated README file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# 03-Homework-Oct-17-2020
+# 03 JavaScript: Password Generator
+
+## Functions and logic
+
+This application generates a random password based on the safety criteria of the OWASP Foundation (https://www.owasp.org/index.php/Password_special_characters). 
+
+## Use
+
+Accessible application to employees wishing to generate a strong password for greater security.
+
+
+## How to use it
+
+1. When you click the GENERATE PASSWORD button, the user will receive a prompt asking for the number of 
+of characters the password will have. The minimum length is 8 characters and the maximum allowed is 128.
+
+2. If the user inputs a number outside the minimum/maximum length, then the application will repreat the prompt.
+
+3. After, the user will receive confirmation prompts for the special characters that the password will have: they can choose lowercase letters, uppercase letters, numeric values, and/or special characters. At least one criteria has to be filled in order for the password to be stronger.
+
+4. After the criteria is set, the application will generate the password on the page. 
+
+I based the application on a github repository. I rewrite the code with IF statements (instead of WHILE loops) and added an additional prompt that informs the user they need to input a valid number if they input a letter by mistake in the first prompt.
+Original credit goes to Tiffany Casey: https://github.com/TiffanyCasey/Password-Generator
+
+You can see the application's functionality in this GIF: ./Password Generator.gif
--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// I used the solution provided in this repository. I tried rewriting the code with IF statements (instead of while) and objects instead of separate arrays, but I couldn't make it work.
-// Credit goes to Tiffany Casey https://github.com/TiffanyCasey/Password-Generator
+// I used the solution provided in a github repository. I rewrite the code with IF statements (instead of WHILE loops) and added an additional prompt that informs the user they need to input a valid number if they input a letter by mistake in the first prompt.
+// Credit goes to Tiffany Casey for the base code: https://github.com/TiffanyCasey/Password-Generator
 
 //Assignment Code + Event Listener to prompt questions when button pushed
-var clickButton = document.querySelector(".btn");
-
+clickButton = document.querySelector("#generate")
 clickButton.addEventListener("click", writePassword);
 
 // Arrays with numbers and strings 
@@ -22,20 +21,19 @@ var answerUpperCase;
 var answerLowerCase;
 
 // Prompt to confirm the number of characters input by the user
+
 function generatePassword() {
-    var answerLength = (prompt("How many characters would you like your password to contain?"));
-
+    
+  var answerLength = (prompt("How many characters would you like your password to contain?"));
   // Loop if answer is outside the parameters 
-    while(answerLength <= 8 || answerLength >= 128) {
-      alert("Password length must be between 8-128 characters Try again");
-      var answerLength = (prompt("How many characters would you like your password to contain?"));
-      
-    }
-
+    if (answerLength <= 8 || answerLength >= 128) {
+      alert("Password length must be between 8-128 characters. Try again");
+      var answerLength = (prompt("How many characters would you like your password to contain?")); 
+    } else {
+  
     // Declare number of characters input by user
-      
       alert("Your password will have " + answerLength + " characters");
-
+    }
 
     // Determine parameters of password stablished by user
       var answerSpecialChara = confirm("Would you like your password to include special characters?");
@@ -44,7 +42,7 @@ function generatePassword() {
       var answerUpperCase = confirm("Would you like your password to include uppercase characters?");
       
     // Loop if prompts do not include at least one criteria 
-      while (answerUpperCase === false && answerLowerCase === false && answerSpecialChara === false && answerNumbers === false) {
+      if (answerUpperCase === false && answerLowerCase === false && answerSpecialChara === false && answerNumbers === false) {
         alert("You must choose at least one parameter");
         var answerSpecialChara = confirm("Would you like your password to include special characters?");
         var answerNumbers = confirm("Would you like your password to include numeric characters?");    
